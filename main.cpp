9ec97b30a71dc15bb4ff2036487623d849f8d227
OleksandrKorkach <oleksandr.korkach@bazucompany.com>
2024-10-20 22:16:34 +0300

    reinit

--- main.cpp
+++ main.cpp
@@ -3,218 +3,314 @@
 #include <string>
 #include <cctype>
 #include <unordered_set>
+#include <utility>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-enum class TokenType {
-    Number,
-    StringLiteral,
-    Comment,
-    ReservedWord,
-    Operator,
-    Punctuation,
-    Identifier,
-    Error
+enum TokenType {
+    KEYWORD,
+    IDENTIFIER,
+
+    INTEGER_LITERAL,
+    LONG_LITERAL,
+    FLOAT_LITERAL,
+    DOUBLE_LITERAL,
+
+    STRING_LITERAL,
+    CHAR_LITERAL,
+
+    BOOLEAN_LITERAL,
+    NULL_LITERAL,
+
+    OPERATOR,
+    SEPARATOR,
+    COMMENT,
+    UNKNOWN
 };
+
+std::string getTokenTypeName(const TokenType type) {
+    switch (type) {
+        case KEYWORD: return "KEYWORD";
+        case IDENTIFIER: return "IDENTIFIER";
+        case INTEGER_LITERAL: return "INTEGER_LITERAL";
+        case LONG_LITERAL: return "LONG_LITERAL";
+        case FLOAT_LITERAL: return "FLOAT_LITERAL";
+        case DOUBLE_LITERAL: return "DOUBLE_LITERAL";
+        case STRING_LITERAL: return "STRING_LITERAL";
+        case CHAR_LITERAL: return "CHAR_LITERAL";
+        case BOOLEAN_LITERAL: return "BOOLEAN_LITERAL";
+        case NULL_LITERAL: return "NULL_LITERAL";
+        case OPERATOR: return "OPERATOR";
+        case SEPARATOR: return "SEPARATOR";
+        case COMMENT: return "COMMENT";
+        case UNKNOWN: return "UNKNOWN";
+        default: return "UNKNOWN";
+    }
+}
 
 struct Token {
-    string lexeme;
+    std::string value;
     TokenType type;
+
+    Token(string v, const TokenType t) : value(std::move(v)), type(t) {}
 };
 
-unordered_set<string> reservedWords = {
-    "break", "case", "catch", "class", "const", "continue", "debugger", "default",
-    "delete", "do", "else", "export", "extends", "finally", "for", "function",
-    "if", "import", "in", "instanceof", "new", "return", "super", "switch",
-    "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield",
-    // ES6
-    "let", "static", "enum", "await"
-};
+unordered_set<string> reservedKeywords = {
+        "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char",
+        "class", "continue", "const", "default", "do", "double", "else", "enum",
+        "exports", "extends", "final", "finally", "float", "for", "goto", "if",
+        "implements", "import", "instanceof", "int", "interface", "long", "module", "native", "new",
+        "package", "private", "protected", "public", "requires", "return", "short", "static", "strictfp",
+        "super", "switch", "synchronized", "this", "throw", "throws", "transient", "try",
+        "var", "void", "volatile", "while",
+    };
 
 unordered_set<string> operators = {
-    "+", "-", "*", "/", "%", "++", "--", "==", "!=", "===", "!==", ">", "<", ">=", "<=",
-    "&&", "||", "!", "&", "|", "^", "~", "<<", ">>", ">>>", "?", ":", "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", ">>>="
+    "+", "-", "*", "/", "=", "==", "!=", "<", ">", "<=", ">=", "&&", "||", "!", "++", "--"
 };
 
-unordered_set<char> punctuation = {
-    '(', ')', '{', '}', '[', ']', ';', ',', '.', ':'
+unordered_set<string> separators = {
+    "(", ")", "{", "}", "[", "]", ";", ",", "."
 };
 
-bool isIdentifierStart(char c) {
-    return isalpha(c) || c == '_' || c == '$';
+unordered_set<string> booleanLiterals = { "true", "false" };
+
+unordered_set<string> nullLiteral = { "null" };
+
+TokenType classifyComment(const std::string& token) {
+    if (token.substr(0, 2) == "//") {
+        return COMMENT;
+    }
+
+    if (token.substr(0, 2) == "/*" && token.substr(token.size() - 2) == "*/") {
+        return COMMENT;
+    }
+
+    return UNKNOWN;
 }
 
-bool isIdentifierPart(char c) {
-    return isalnum(c) || c == '_' || c == '$';
+TokenType classifyKeyword(const std::string& token) {
+    if (reservedKeywords.contains(token)) {
+        return KEYWORD;
+    }
+    return UNKNOWN;
 }
 
-vector<Token> tokenize(const string& code) {
-    vector<Token> tokens;
-    size_t i = 0;
-    size_t length = code.length();
+TokenType classifySeparator(const std::string& token) {
+    if (separators.contains(token)) {
+        return SEPARATOR;
+    }
+    return UNKNOWN;
+}
 
-    while (i < length) {
-        char c = code[i];
+TokenType classifyOperator(const std::string& token) {
+    if (operators.contains(token)) {
+        return OPERATOR;
+    }
+    return UNKNOWN;
+}
+
+TokenType classifyNumericLiteral(const std::string& token) {
+    if (token.back() == 'L' || token.back() == 'l') {
+        return LONG_LITERAL;
+    }
+    if (token.back() == 'F' || token.back() == 'f') {
+        return FLOAT_LITERAL;
+    }
+    if (token.find('.') != std::string::npos) {
+        return DOUBLE_LITERAL;
+    }
+    return INTEGER_LITERAL;
+}
+
+TokenType classifyLiteral(const std::string& token) {
+    if (token.size() >= 2 && token.front() == '"' && token.back() == '"') {
+        return STRING_LITERAL;
+    }
+    if (token.size() == 3 && token.front() == '\'' && token.back() == '\'') {
+        return CHAR_LITERAL;
+    }
+    if (token.size() == 4 && token.front() == '\'' && token.back() == '\'' && token[1] == '\\') {
+        return CHAR_LITERAL;
+    }
+    return UNKNOWN;
+}
+
+TokenType classifyBooleanLiteral(const std::string& token) {
+    if (booleanLiterals.contains(token)) {
+        return BOOLEAN_LITERAL;
+    }
+    return UNKNOWN;
+}
+
+TokenType classifyNullLiteral(const std::string& token) {
+    if (token == "null") {
+        return NULL_LITERAL;
+    }
+    return UNKNOWN;
+}
+
+TokenType classifyToken(const std::string& token) {
+    if (const TokenType commentType = classifyComment(token); commentType == COMMENT) {
+        return COMMENT;
+    }
+
+    if (const TokenType separatorType = classifySeparator(token); separatorType == SEPARATOR) {
+        return SEPARATOR;
+    }
+
+    if (const TokenType operatorType = classifyOperator(token); operatorType == OPERATOR) {
+        return OPERATOR;
+    }
+
+    if (const TokenType stringLiteralType = classifyLiteral(token); stringLiteralType == STRING_LITERAL) {
+        return STRING_LITERAL;
+    }
+
+    if (const TokenType charLiteralType = classifyLiteral(token); charLiteralType == CHAR_LITERAL) {
+        return CHAR_LITERAL;
+    }
+
+    if (const TokenType booleanLiteralType = classifyBooleanLiteral(token); booleanLiteralType == BOOLEAN_LITERAL) {
+        return BOOLEAN_LITERAL;
+    }
+
+    if (const TokenType nullLiteralType = classifyNullLiteral(token); nullLiteralType == NULL_LITERAL) {
+        return NULL_LITERAL;
+    }
+
+    if (isdigit(token.front())) {
+        return classifyNumericLiteral(token);
+    }
+
+    if (const TokenType keywordType = classifyKeyword(token); keywordType == KEYWORD) {
+        return KEYWORD;
+    }
+
+    if (isalpha(token.front()) || token.front() == '_') {
+        return IDENTIFIER;
+    }
+
+    return UNKNOWN;
+}
+
+vector<string> splitIntoTokens(const string& line) {
+    vector<string> tokens;
+    string currentToken;
+
+    for (size_t i = 0; i < line.size(); ++i) {
+        char c = line[i];
 
         if (isspace(c)) {
-            i++;
-            continue;
-        }
-
-        if (c == '/') {
-            if (i + 1 < length) {
-                if (code[i + 1] == '/') {
-                    size_t start = i;
-                    i += 2;
-                    while (i < length && code[i] != '\n') {
-                        i++;
-                    }
-                    tokens.push_back({code.substr(start, i - start), TokenType::Comment});
-                    continue;
-                } else if (code[i + 1] == '*') {
-                    size_t start = i;
-                    i += 2;
-                    while (i + 1 < length && !(code[i] == '*' && code[i + 1] == '/')) {
-                        i++;
-                    }
-                    if (i + 1 >= length) {
-                        tokens.push_back({code.substr(start), TokenType::Error});
-                        break;
-                    }
-                    i += 2;
-                    tokens.push_back({code.substr(start, i - start), TokenType::Comment});
-                    continue;
-                }
-            }
-        }
-
-        if (c == '"' || c == '\'') {
-            char quoteType = c;
-            size_t start = i;
-            i++;
-            while (i < length && code[i] != quoteType) {
-                if (code[i] == '\\') {
-                    i++;
-                }
-                i++;
-            }
-            if (i >= length) {
-                tokens.push_back({code.substr(start), TokenType::Error});
-                break;
-            }
-            i++;
-            tokens.push_back({code.substr(start, i - start), TokenType::StringLiteral});
-            continue;
-        }
-
-        if (isdigit(c) || (c == '.' && i + 1 < length && isdigit(code[i + 1]))) {
-            size_t start = i;
-            bool isHex = false;
-            if (c == '0' && i + 1 < length && (code[i + 1] == 'x' || code[i + 1] == 'X')) {
-                isHex = true;
-                i += 2;
-                while (i < length && isxdigit(code[i])) {
-                    i++;
-                }
-            } else {
-                while (i < length && isdigit(code[i])) {
-                    i++;
-                }
-                if (i < length && code[i] == '.') {
-                    i++;
-                    while (i < length && isdigit(code[i])) {
-                        i++;
-                    }
-                }
-                if (i < length && (code[i] == 'e' || code[i] == 'E')) {
-                    i++;
-                    if (i < length && (code[i] == '+' || code[i] == '-')) {
-                        i++;
-                    }
-                    while (i < length && isdigit(code[i])) {
-                        i++;
-                    }
-                }
-            }
-            tokens.push_back({code.substr(start, i - start), TokenType::Number});
-            continue;
-        }
-
-        if (isIdentifierStart(c)) {
-            size_t start = i;
-            i++;
-            while (i < length && isIdentifierPart(code[i])) {
-                i++;
-            }
-            string lexeme = code.substr(start, i - start);
-            if (reservedWords.count(lexeme)) {
-                tokens.push_back({lexeme, TokenType::ReservedWord});
-            } else {
-                tokens.push_back({lexeme, TokenType::Identifier});
+            if (!currentToken.empty()) {
+                tokens.push_back(currentToken);
+                currentToken.clear();
             }
             continue;
         }
 
-        bool matchedOperator = false;
-        for (size_t opLen = 3; opLen > 0; opLen--) {
-            if (i + opLen <= length) {
-                string op = code.substr(i, opLen);
-                if (operators.count(op)) {
-                    tokens.push_back({op, TokenType::Operator});
-                    i += opLen;
-                    matchedOperator = true;
-                    break;
+        if (separators.contains(string(1, c))) {
+            if (!currentToken.empty()) {
+                tokens.push_back(currentToken);
+                currentToken.clear();
+            }
+            tokens.emplace_back(1, c);
+            continue;
+        }
+
+        if (i < line.size() - 1) {
+            string twoCharOperator = string(1, c) + line[i + 1];
+            if (operators.contains(twoCharOperator)) {
+                if (!currentToken.empty()) {
+                    tokens.push_back(currentToken);
+                    currentToken.clear();
                 }
+                tokens.push_back(twoCharOperator);
+                ++i;
+                continue;
             }
         }
-        if (matchedOperator) {
+
+        if (operators.contains(string(1, c))) {
+            if (!currentToken.empty()) {
+                tokens.push_back(currentToken);
+                currentToken.clear();
+            }
+            tokens.emplace_back(1, c);
             continue;
         }
 
-        if (punctuation.count(c)) {
-            tokens.push_back({string(1, c), TokenType::Punctuation});
-            i++;
+        if (isdigit(c)) {
+            currentToken += c;
+            if (i + 1 < line.size() && line[i + 1] == '.' && i + 2 < line.size() && isdigit(line[i + 2])) {
+                currentToken += line[i + 1];
+                ++i;
+                while (i + 1 < line.size() && isdigit(line[i + 1])) {
+                    currentToken += line[++i];
+                }
+            }
             continue;
         }
 
-        tokens.push_back({string(1, c), TokenType::Error});
-        i++;
+        if (c == '"') {
+            if (!currentToken.empty()) {
+                tokens.push_back(currentToken);
+                currentToken.clear();
+            }
+            currentToken += c;
+            ++i;
+            while (i < line.size() && line[i] != '"') {
+                currentToken += line[i++];
+            }
+            currentToken += '"';
+            tokens.push_back(currentToken);
+            currentToken.clear();
+            continue;
+        }
+
+        if (isalpha(c) || c == '_') {
+            currentToken += c;
+            while (i + 1 < line.size() && (isalnum(line[i + 1]) || line[i + 1] == '.' || line[i + 1] == '_')) {
+                currentToken += line[++i];
+            }
+            tokens.push_back(currentToken);
+            currentToken.clear();
+            continue;
+        }
+
+        currentToken += c;
+    }
+
+    if (!currentToken.empty()) {
+        tokens.push_back(currentToken);
     }
 
     return tokens;
 }
 
-string tokenTypeToString(TokenType type) {
-    switch (type) {
-        case TokenType::Number: return "Number";
-        case TokenType::StringLiteral: return "StringLiteral";
-        case TokenType::Comment: return "Comment";
-        case TokenType::ReservedWord: return "ReservedWord";
-        case TokenType::Operator: return "Operator";
-        case TokenType::Punctuation: return "Punctuation";
-        case TokenType::Identifier: return "Identifier";
-        case TokenType::Error: return "Error";
-        default: return "Unknown";
-    }
+void printFormattedToken(const std::string& token, TokenType type) {
+    cout << left << setw(20) << token;
+    cout << " | " << getTokenTypeName(type) << endl;
 }
 
 int main() {
-    string code = R"(
+    ifstream file("test.txt");
+    string line;
 
-    // JavaScript
-    var x = 42;
-    function greet(name) {
-        console.log("Hello, " + name);
+    if (!file) {
+        cerr << "Error: Unable to open file 'test.txt'" << endl;
+        return 1;
     }
 
-    )";
-
-    vector<Token> tokens = tokenize(code);
-
-    for (const auto& token : tokens) {
-        cout << "< " << token.lexeme << " , " << tokenTypeToString(token.type) << " >" << endl;
+    while (getline(file, line)) {
+        for (vector<string> tokens = splitIntoTokens(line); const string& token : tokens) {
+            const TokenType type = classifyToken(token);
+            printFormattedToken(token, type);
+        }
     }
 
+    file.close();
     return 0;
 }
